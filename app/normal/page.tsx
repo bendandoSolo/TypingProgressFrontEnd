3c233bb8e350a9abb1f2c7e29619d9aaa2620602
Ben Roberts <bendando@hotmail.co.uk>
2024-09-14 12:38:14 +0100

    eslint with plugin react recommended

---
+++ app/normal/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NormalPage = () => {
+    return (
+        <div>
+            Here we are going to show the graph of the normal data..
+        </div>
+    );
+};
+
+export default NormalPage;
--- app/page.tsx
+++ app/page.tsx
@@ -7,17 +7,32 @@ export default function Home() {
 
   const [data, setData] = useState([]);
 
-  const getAll = async () => {
+  useEffect(() => {
+    data.length === 0 && getAll();
+  }, [data])
+  
+
+  const getAll = async (): Promise<void> => {
     const response = await fetch("https://localhost:7015/typingData");
     const data = await response.json();
     console.log(data);
-    alert(JSON.stringify(data));
     setData(data);
   };
 
   return (
     <main className={styles.main}>
+       <ul>
+          <li>use MUI</li>
+          <li>Separate out fetching data, to services, make it auto fetch, consider react query</li>
+          <li>Hide and Show data sections with accordion</li>
+          <li>Graph results</li>
+          <li>Use a table to display data</li>
+          <li>Basic stats analysis over data</li>
+        </ul>
+      <br />
+      <br />
       <div className={styles.description}>
+       
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
