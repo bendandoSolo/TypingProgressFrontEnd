02feb446923b05c8bc8643ee953551e73c89edb0
Ben Roberts <bendando@hotmail.co.uk>
2024-12-29 13:21:40 +0000

    implement basic prettier on save settings

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,24 +1,27 @@
-import AccordionView from "@/_components/AccordionView";
+import AccordionView from '@/_components/AccordionView';
 import { AccordionData, AccordionViewProps } from '@/_types/AccordionData';
-
 
 // should be in a seperate file
 const testData: AccordionData[] = [
-  { title: 'Test 1', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing' },
+  {
+    title: 'Test 1',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing',
+  },
   { title: '2', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing' },
-  { title: '33', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing' },
+  {
+    title: '33',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing',
+  },
 ];
 
-
-
 const Test = () => {
-    return (
-        <div>
-            <h1>Testing the current Components</h1>
-            1 Accordion...
-            <AccordionView data={testData}/>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Testing the current Components</h1>
+      1 Accordion...
+      <AccordionView data={testData} />
+    </div>
+  );
 };
 
 export default Test;
